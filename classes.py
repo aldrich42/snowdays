85f92a6741a4376d83681feb21c92063c29c1c4e
aldrich42 <lochlan.aldrich@gmail.com>
2025-01-14 17:40:03 -0500

    added class Municipality

--- classes.py
+++ classes.py
@@ -21,6 +21,11 @@ class Zone(object):
         pass
 
 
+class Municipality(object):
+    def __init__(self):
+        pass
+
+
 class Place(object):
     def __init__(self, latitude: str, longitude: str, municipality: str | None = None, state: str | None = None,
                  wfo: str | None = None, x: str | None = None, y: str | None = None,
