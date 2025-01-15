c70fddaa541bc408bbb94fbfe497ddce9938f306
aldrich42 <lochlan.aldrich@gmail.com>
2025-01-14 19:34:23 -0500

    added Location.__init__

--- classes.py
+++ classes.py
@@ -1,11 +1,26 @@
 from api_functions import call_json
 
 
+headers = None
+
+
 class Point(object):
-    def __init__(self, latlon: str):
+    def __init__(self, latlon_str: str):
         self.latitude: str
         self.longitude: str
-        self.latitude, self.longitude = tuple(latlon.split(","))
+        self.latitude, self.longitude = tuple(latlon_str.split(","))
+
+    def get_grid_data(self):
+        url = f"https://api.weather.gov/points/{self.latitude},{self.longitude}"
+        data = call_json(url, headers=headers)["properties"]
+        return GridPoint(data["relativeLocation"]["properties"]["city"],
+                         data["relativeLocation"]["properties"]["state"],
+                         data["gridId"], data["gridX"], data["gridY"])
+
+    def get_zone(self):
+        url = f"https://api.weather.gov/zones?type=land&point={self.latitude},{self.longitude}&include_geometry=false"
+        data = call_json(url)["features"][0]["properties"]
+        return Zone(data["id"], data["name"])
 
 
 class GridPoint(object):
@@ -15,6 +30,11 @@ class GridPoint(object):
         self.wfo: str = wfo
         self.grid_x: str = grid_x
         self.grid_y: str = grid_y
+
+    def get_station(self):
+        url = f"https://api.weather.gov/gridpoints/{self.wfo}/{self.grid_x},{self.grid_y}/stations"
+        data = call_json(url)["features"][0]["properties"]
+        return Station(data["stationIdentifier"], data["name"])
 
 
 class Zone(object):
@@ -117,8 +137,24 @@ class Place(object):
 
 
 class Location(object):
-    def __init__(self):
-        pass
+    def __init__(self, latlon: Point, grid_data: GridPoint | None = None, zone: Zone | None = None,
+                 station: Station | None = None):
+        self.latlon: Point = latlon
+        self.grid_data: GridPoint
+        self.zone: Zone
+        self.station: Station
+        if grid_data is None:
+            self.grid_data = latlon.get_grid_data()
+        else:
+            self.grid_data = grid_data
+        if zone is None:
+            self.zone = latlon.get_zone()
+        else:
+            self.zone = zone
+        if station is None:
+            self.station = grid_data.get_station()
+        else:
+            self.station = station
 
 
 class District(object):
