3ade22f45fbb1e4e405340e2c0fcda23aedaa826
aldrich42 <lochlan.aldrich@gmail.com>
2025-01-14 21:56:53 -0500

    made a better example

--- main.py
+++ main.py
@@ -21,12 +21,10 @@ def fmt(value: float) -> str:
 def main():
     if check_ok():
         test_district: District = District(
-            Location(Point("40.7128,-74.0060")),  # new york
-            Location(Point("34.0549,-118.2426")),  # los angeles
-            Location(Point("41.8781,-87.6298")),  # chicago
-            Location(Point("29.7601,-95.3701")),  # houston
-            Location(Point("33.4484,-112.0740")),  # phoenix
-            Location(Point("39.9526,-75.1652")),  # philadelphia
+            Location(Point("42.3555,-71.0565"),
+                     grid_data=GridPoint("Boston", "MA", "BOX", "72", "90", "KBOX"),
+                     zone=Zone("MAZ025", "Suffolk"),
+                     station=Station("KBOS", "Boston, Logan International Airport")),  # boston
         )
         print("accessed locations")
         print(fmt(chance_of_snow_day(test_district)))
