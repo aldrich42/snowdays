7def3469d1899a94ba3b7ec167a8078914a56071
aldrich42 <lochlan.aldrich@gmail.com>
2025-01-14 21:32:27 -0500

    never using kelvin

--- functions.py
+++ functions.py
@@ -3,30 +3,16 @@ import numpy as np
 
 
 def convert_temp(value: float, from_unit: str, to_unit: str) -> float:
-    # temp units: K, C, F
-
-    def k_to_c(k: float) -> float:
-        return k - 273.15
-
-    def k_to_f(k: float) -> float:
-        return (k - 273.15) * 9/5 + 32
-
-    def c_to_k(c: float) -> float:
-        return c + 273.15
+    # temp units: C, F
 
     def c_to_f(c: float) -> float:
         return c * 9/5 + 32
-
-    def f_to_k(f: float) -> float:
-        return (f - 32) * 5/9 + 273.15
 
     def f_to_c(f: float) -> float:
         return (f - 32) * 5/9
 
     conversion_table: dict = {
-        ("K", "C"): k_to_c, ("K", "F"): k_to_f,
-        ("C", "K"): c_to_k, ("C", "F"): c_to_f,
-        ("F", "K"): f_to_k, ("F", "C"): f_to_c,
+        ("C", "F"): c_to_f, ("F", "C"): f_to_c,
     }
 
     try:
