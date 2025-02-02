7ed302f65658be4c3788c93278cdbc5aba40ea38
aldrich42 <lochlan.aldrich@gmail.com>
2025-02-02 00:23:59 -0500

    created some more work for myself again :P :P

--- main.py
+++ main.py
@@ -192,54 +192,9 @@ class District(object):
         self.center: Location = center
         self.margin: tuple[Location, ...] = margin
 
-def convert_temp(value: float, from_unit: str, to_unit: str) -> float:
-    # temp units: C, F
 
-    def c_to_f(c: float) -> float:
-        return c * 9/5 + 32
-
-    def f_to_c(f: float) -> float:
-        return (f - 32) * 5/9
-
-    conversion_table: dict = {
-        ("C", "F"): c_to_f, ("F", "C"): f_to_c,
-    }
-
-    try:
-        return conversion_table[(from_unit, to_unit)](value)
-    except KeyError:
-        raise ValueError(f"Unknown temperature conversion: {(from_unit, to_unit).__repr__()}")
-
-
-def convert_height(value: float, from_unit: str, to_unit: str) -> float:
-    # height units: cm, m, in, ft
-
-    conversion_table: dict = {
-        ("cm", "m"): 1/100, ("cm", "in"): 1/2.54, ("cm", "ft"): 1/(2.54*12),
-        ("m", "cm"): 100, ("m", "in"): 100/2.54, ("m", "ft"): 100/(12*2.54),
-        ("in", "cm"): 2.54, ("in", "m"): 2.54/100, ("in", "ft"): 1/12,  # todo standard nws units, also mm
-        ("ft", "cm"): 12*2.54, ("ft", "m"): 12*2.54/100, ("ft", "in"): 12
-    }
-
-    try:
-        return value * conversion_table[(from_unit, to_unit)]
-    except KeyError:
-        raise ValueError(f"Unknown height conversion: {(from_unit, to_unit).__repr__()}")
-
-
-def convert_speed(value: float, from_unit: str, to_unit: str) -> float:
-    # height units: mps, mph
-
-    conversion_table: dict = {
-        ("mps", "mph"): 5280*(12*2.54/100)/3600,  # todo kph <-> mph instead of mps
-        ("mph", "mps"): 3600*(100/(12*2.54))/5280
-    }
-
-    try:
-        return value * conversion_table[(from_unit, to_unit)]
-    except KeyError:
-        raise ValueError(f"Unknown height conversion: {(from_unit, to_unit).__repr__()}")
-
+def convert():
+    pass
 
 def neural_net():
     pass
